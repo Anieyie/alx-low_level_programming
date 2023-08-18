8c4218c544d5d22e23932085e31103ab7083855d
Anieyie <godsglorifiedanieyie@gmail.com>
2023-08-18 11:27:20 -0700

    second try

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 
-* main - causes an infinite loop
+* main - Test function for positive or negative
 
 * Return: 0
 
@@ -14,19 +14,9 @@ int main(void)
 
 int i;
 
-printf("Infinite loop incoming :(\n");
-
 i = 0;
 
-/*while (i < 10)*/
-
-/*{*/
-
-/*putchar(i);*/
-
-/*}*/
-
-printf("Infinite loop avoided! \\o/\n");
+positive_or_negative(i);
 
 return (0);
 
